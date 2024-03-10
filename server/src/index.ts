46f090fcd54901ffa943577836c76db6d46883f8
Felix <jonfelixrico@gmail.com>
2024-03-10 21:41:38 +0800

    Install nanoid

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,22 @@ require('dotenv').config({
 })
 
 import express from 'express'
+import { nanoid } from 'nanoid'
 
 const app = express()
 
 app.get('/', (_, res) => {
   res.send('Hello World')
+})
+
+const roomObjects: Record<string, Object> = {}
+app.post('/room', (_, res) => {
+  const id = nanoid()
+  roomObjects[id]
+  
+  res.json({
+    id
+  })
 })
 
 app.listen(3000)
