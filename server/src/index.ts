8be7813bf7220649ea38e25f29c753639fbd03c0
Felix <jonfelixrico@gmail.com>
2024-03-10 17:20:52 +0800

    Install dotenv

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+require('dotenv').config({
+  path: ['.env.local', '.env']
+})
+
+import express from 'express'
+
+const app = express()
+
+app.get('/', (_, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000)
