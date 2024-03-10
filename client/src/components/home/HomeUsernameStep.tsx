99b7004d6085ea6cc6d6b418c236a2e3f6f72a2a
Felix <jonfelixrico@gmail.com>
2024-03-10 19:59:29 +0800

    Flesh out the component

--- client/src/components/home/HomeUsernameStep.tsx
+++ client/src/components/home/HomeUsernameStep.tsx
@@ -2,13 +2,31 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Stack from 'react-bootstrap/Stack'
 import Card from 'react-bootstrap/Card'
+import { useState } from 'react'
 
-export default function HomeUsernameStep () {
+export default function HomeUsernameStep (props: {
+  onInput: (value: string) => void
+}) {
+  const [value, setValue] = useState('')
+
   return <Card>
     <Card.Body>
       <Stack gap={1}>
-        <Form.Control placeholder='Pick a name' />
-        <Button>Confirm and Submit</Button>
+        <Form.Control
+          placeholder='Pick a name'
+          name="name"
+          type='text'
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        
+        <Button
+          type='submit'
+          disabled={!value}
+          onSubmit={() => props.onInput(value)}
+        >
+          Confirm Name
+        </Button>
       </Stack>
     </Card.Body>
   </Card>
