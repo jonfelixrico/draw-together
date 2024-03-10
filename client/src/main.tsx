523e5bcb1bf3fd40aeb94a390528863cecf2b196
Felix <jonfelixrico@gmail.com>
2024-03-10 18:13:39 +0800

    Remove app.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './index.css'
-import App from './App.tsx'
 import ReactRouterProvider from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ReactRouterProvider />
-    <App />
   </React.StrictMode>,
 )
