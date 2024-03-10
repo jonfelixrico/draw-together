b589fa5641ce92ee02ce8f77b526d550406dc97f
Felix <jonfelixrico@gmail.com>
2024-03-10 23:07:33 +0800

    Add comment

--- client/src/hooks/api-client.ts
+++ client/src/hooks/api-client.ts
@@ -1,6 +1,12 @@
 import { useMemo } from "react";
 import { getApiClient } from "../services/api-client";
 
+/**
+ * Use this over `getApiClient` inside of components.
+ * We prefer to use this over `getApiClient` because using the latter
+ * will keep on calling `getApiClient` needlessly during re-render.
+ * @returns 
+ */
 export function useApiClient () {
   return useMemo(() => getApiClient(), [])
 }
