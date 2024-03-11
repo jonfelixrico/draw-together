c4a61d2463b168c49872a468a675705276422a6b
Felix <jonfelixrico@gmail.com>
2024-03-11 20:43:11 +0800

    Distinguish the errors

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -2,11 +2,26 @@ import { Container } from "react-bootstrap";
 import { getApiClient } from "../services/api-client";
 import { LoaderFunction, useParams } from "react-router-dom";
 
+enum RoomErrorType {
+  NO_ROOM_ID,
+  NO_USERNAME
+}
+
+class RoomError extends Error {
+  constructor(public type: RoomErrorType) {
+    super(type.toString())
+  }
+}
+
 export const loader: LoaderFunction = async ({ params }) => {
   const api = getApiClient()
 
   if (!params.roomId) {
-    throw new Error('No room id')
+    throw new RoomError(RoomErrorType.NO_ROOM_ID)
+  }
+
+  if (!window.localStorage.getItem('username')) {
+    throw new RoomError(RoomErrorType.NO_USERNAME)
   }
 
   return await api.get(`room/${params.roomId}`)
