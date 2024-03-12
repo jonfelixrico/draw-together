53135e38c6325fed714bef70fb0db6688b825952
Felix <jonfelixrico@gmail.com>
2024-03-12 22:14:42 +0800

    Create util for getClientUUID

---
+++ client/src/utils/local-storage.util.ts
@@ -0,0 +1,14 @@
+import { nanoid } from 'nanoid'
+
+const CLIENT_UUID_LS_KEY = 'uuid'
+
+export function getClientUUID() {
+  let uuid = window.localStorage.getItem(CLIENT_UUID_LS_KEY)
+  if (uuid) {
+    return uuid
+  }
+
+  uuid = nanoid()
+  window.localStorage.setItem('uuid', CLIENT_UUID_LS_KEY)
+  return uuid
+}
