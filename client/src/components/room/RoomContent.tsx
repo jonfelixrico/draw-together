5140500f8ebaccdafd9b8d4e2731f31decb9dc41
Felix <jonfelixrico@gmail.com>
2024-03-12 21:30:01 +0800

    Use loader data

--- client/src/components/room/RoomContent.tsx
+++ client/src/components/room/RoomContent.tsx
@@ -1,10 +1,21 @@
 import Container from 'react-bootstrap/Container'
-import { useParams } from 'react-router-dom'
+import { useLoaderData, useParams } from 'react-router-dom'
+import { Room } from '../../typings/room.types'
+import { Socket } from 'socket.io-client'
+
+interface LoaderData {
+  room: Room
+  socket: Socket
+}
 
 export default function RoomContent () {
   const params = useParams<{ roomId: string }>()
+  const {
+    room,
+    socket
+  } = useLoaderData() as LoaderData
 
   return <Container>
-    {props.roomId}
+    {params.roomId}
   </Container>
 }
