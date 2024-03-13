06e4d1bc1127bf4e2b56667ab5ef42854482625e
Felix <jonfelixrico@gmail.com>
2024-03-13 20:11:18 +0800

    Improve connect/disconnect logic

--- server/src/socket-io.ts
+++ server/src/socket-io.ts
@@ -9,13 +9,27 @@ function getQSData ({ request }: Socket) {
   return {
     // TODO use the `qs` library for this
     roomId: url.searchParams.get('roomId'),
-    clientId: url.searchParams.get('clientId') || 'UNKNOWN'
+    clientId: url.searchParams.get('clientId') || 'UNKNOWN_ID',
+    name: url.searchParams.get('name') || 'UNKNOWN_NAME'
+  }
+}
+
+interface ConnectedClient {
+  id: string
+  name: string
+}
+
+type RoomDict = {
+  [roomId: string]: {
+    [clientId: string]: ConnectedClient
   }
 }
 
 export function socketIOHandler (io: Server) {
+  const rooms: RoomDict = {}
+
   io.on('connect', (socket: Socket) => {
-    const { roomId, clientId } = getQSData(socket)
+    const { roomId, clientId, name } = getQSData(socket)
   
     console.debug('Socket %s has connected', clientId)
   
@@ -27,16 +41,64 @@ export function socketIOHandler (io: Server) {
     }
   
     // TODO add checking for unknown clientId
+    // TODO add checking for unknown name
   
     // TODO add checking for nonexistent rooms
-  
+
     socket.join(roomId)
+    socket.broadcast.to(roomId).emit('BROADCAST', {
+      code: 'CONN_USER',
+      payload: {
+        clientId,
+        action: 'join'
+      }
+    })
     console.debug('Connected socket %s to room %s', clientId, roomId)
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = {}
+    }
+
+    if (!rooms[roomId][clientId]) {
+      rooms[roomId][clientId] = {
+        id: clientId,
+        name
+      }
+    }
+
+    socket.on('disconnect', () => {
+      io.to(roomId).emit('BROADCAST', {
+        code: 'CONN_USER',
+        payload: {
+          clientId,
+          action: 'leave'
+        }
+      })
+      delete rooms[roomId][clientId]
+
+      console.debug('Client %s has left', clientId)
+    })
+
+
+    socket.on('SERVER_REQ', ({ code }: { code: string }, respond: (response: unknown) => void) => {
+      switch (code) {
+        case 'CONN_LIST': {
+          respond(Object.values(rooms[roomId]))
+          console.debug('Sent %s the list of connected participants', clientId)
+          break
+        }
+
+        default: {
+          console.debug('Unknown SERVER_REQ code: %s', code)
+          respond(null)
+        }
+      }
+    })
   
     // The purpose of this is to just relay the message to all participants
     socket.on('BROADCAST', (payload: Record<string, unknown>) => {
       console.debug('Relayed %o to room %s, from client %s', payload, roomId, clientId)
-      socket.to(roomId).emit('BROADCAST', payload)
+      socket.broadcast.to(roomId).emit('BROADCAST', payload)
     })
   })
 }
