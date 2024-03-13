98ef791cf4eb890cec26f28bbfb263b4fa03ccb4
Felix <jonfelixrico@gmail.com>
2024-03-13 20:33:18 +0800

    Create manager and socket hooks

--- client/src/components/room/RoomContent.tsx
+++ client/src/components/room/RoomContent.tsx
@@ -1,28 +1,14 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { useRoomSocket } from './room-socket.hook'
-import { JoinedPayload, RoomSocketCode, UserDataPayload } from '../../typings/room-socket-code.types'
-import { getClientUUID } from '../../utils/local-storage-vars.util'
 import { useMount } from '../../hooks/lifecycle.hook'
+import { useRoomSocket } from './room-socket.hook'
 
 export default function RoomContent () {
-  const { broadcastMessage, listenForMessage, socket } = useRoomSocket()
+  const socket = useRoomSocket()
 
   useMount(() => {
-    console.log('Sent init message')
-    broadcastMessage(RoomSocketCode.JOINED, {
-      uuid: getClientUUID()
-    } as JoinedPayload)
-
-    listenForMessage(RoomSocketCode.JOINED, ({ uuid }: JoinedPayload) => {
-      console.log('Received JOINED message from %s. Sending user data.', uuid)
-      broadcastMessage(RoomSocketCode.USER_DATA, {
-        uuid: getClientUUID(),
-        // TODO do something ismilar to getClientUUID
-        name: window.localStorage.getItem('username') as string // casted as string since user can't reach this page without username in LS
-      } as UserDataPayload)
-    })
+    // TODO do something
   })
 
   return <Container>
--- client/src/components/room/room-socket.hook.ts
+++ client/src/components/room/room-socket.hook.ts
@@ -1,28 +1,40 @@
-import { useLoaderData } from "react-router-dom";
-import { Socket } from "socket.io-client";
-import { BroadcastPayload, RoomSocketCode } from "../../typings/room-socket-code.types";
+import { Socket } from "socket.io-client"
+import { BroadcastPayload, RoomSocketCode, RoomSocketEvent } from "../../typings/room-socket-code.types"
+import { useCallback, useEffect, useState } from "react"
+import { useLoaderData } from "react-router-dom"
+
+export function useRoomSocketManager (socket: Socket) {
+  const [lastMessage, setLastMessage] = useState<BroadcastPayload<unknown>>()
+
+  const sendMessage = useCallback((code: RoomSocketCode, payload: unknown) => {
+    socket.emit(RoomSocketEvent.BROADCAST, {
+      code,
+      payload
+    } as BroadcastPayload)
+    console.debug('Sent message with code %s and payload %o', code, payload)
+  }, [socket])
+
+  useEffect(() => {
+    function handler (payload: BroadcastPayload<unknown>) {
+      setLastMessage(payload)
+    }
+
+    socket.on(RoomSocketEvent.BROADCAST, handler)
+    console.debug('Created socket.io listener for %s', RoomSocketEvent.BROADCAST)
+
+    return () => {
+      console.debug('Removed socket.io listener for %s', RoomSocketEvent.BROADCAST)
+      socket.off(RoomSocketEvent.BROADCAST, handler)
+    }
+  }, [socket, setLastMessage])
+
+  return {
+    lastMessage,
+    sendMessage
+  }
+}
 
 export function useRoomSocket () {
   const { socket } = useLoaderData() as { socket: Socket }
-
-  function broadcastMessage (code: RoomSocketCode, payload?: unknown) {
-    socket.emit('BROADCAST', {
-      code,
-      payload
-    })
-  }
-
-  function listenForMessage <T = unknown>(code: RoomSocketCode, handler: (payload: T) => void) {
-    socket.on('BROADCAST', (broadcastPayload: BroadcastPayload<T>) => {
-      if (code === broadcastPayload.code) {
-        handler(broadcastPayload.payload)
-      }
-    })
-  }
-
-  return {
-    socket,
-    broadcastMessage,
-    listenForMessage
-  }
+  return socket
 }
