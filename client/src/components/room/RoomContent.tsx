bc7a1e125c39fe2e7dcc7bf57eeccebdcda3cbb1
Felix <jonfelixrico@gmail.com>
2024-03-13 19:50:24 +0800

    Revert "Remove code for now"
    This reverts commit 90275d61860ac4bcb420960481bd7c14dd50f403.

--- client/src/components/room/RoomContent.tsx
+++ client/src/components/room/RoomContent.tsx
@@ -1,8 +1,38 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { useRoomSocket } from './room-socket.hook'
+import { JoinedPayload, RoomSocketCode, UserDataPayload } from '../../typings/room-socket-code.types'
+import { getClientUUID } from '../../utils/local-storage-vars.util'
+import { useUnmount } from 'react-use'
+import { useMount } from '../../hooks/lifecycle.hook'
 
 export default function RoomContent () {
+  const { broadcastMessage, listenForMessage, socket } = useRoomSocket()
+
+  useUnmount(() => {
+    return () => {
+      console.log('Disconnected')
+      socket.disconnect()
+    }
+  })
+
+  useMount(() => {
+    console.log('Sent init message')
+    broadcastMessage(RoomSocketCode.JOINED, {
+      uuid: getClientUUID()
+    } as JoinedPayload)
+
+    listenForMessage(RoomSocketCode.JOINED, ({ uuid }: JoinedPayload) => {
+      console.log('Received JOINED message from %s. Sending user data.', uuid)
+      broadcastMessage(RoomSocketCode.USER_DATA, {
+        uuid: getClientUUID(),
+        // TODO do something ismilar to getClientUUID
+        name: window.localStorage.getItem('username') as string // casted as string since user can't reach this page without username in LS
+      } as UserDataPayload)
+    })
+  })
+
   return <Container>
     <Row>
       <Col xs="2">
