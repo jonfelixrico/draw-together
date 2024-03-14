58fc17e1d25647daecd8df93f21cc4d68874c1bd
Felix <jonfelixrico@gmail.com>
2024-03-15 00:58:11 +0800

    Revert "Add the use of useMemo"
    This reverts commit 6d0f4a0a920a80cd44852d480341a51155b6e338.

--- client/src/components/interact/Draggable.tsx
+++ client/src/components/interact/Draggable.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, CSSProperties, useMemo } from 'react'
+import { useRef, useEffect, CSSProperties } from 'react'
 import interact from 'interactjs'
 import { Point } from '../../typings/geometry.types'
 
@@ -108,13 +108,11 @@ export default function Draggable ({
     }
   }, [onDrag, skipRate])
 
-  return useMemo(() => {
-    return <div
-      ref={elRef}
-      style={{
-        ...dimensions,
-        cursor
-      }}
-    />
-  }, [dimensions, cursor])
+  return <div
+    ref={elRef}
+    style={{
+      ...dimensions,
+      cursor
+    }}
+  />
 }
