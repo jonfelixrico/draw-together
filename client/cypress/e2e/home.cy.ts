04312666d82916f06256d60f02d66bc07db4f88f
Felix <jonfelixrico@gmail.com>
2024-03-14 18:36:30 +0800

    Add test stub

---
+++ client/cypress/e2e/home.cy.ts
@@ -0,0 +1,17 @@
+describe('home', () => {
+  it('shows username step if no username is found in localstorage', () => {
+    // TODO add test
+  })
+
+  it('shows action step if username is already in localstorage', () => {
+    // TODO add test
+  })
+
+  it('supports hosting', () => {
+    // TODO add test
+  })
+
+  it('supports joining', () => {
+    // TODO add test
+  })
+})
