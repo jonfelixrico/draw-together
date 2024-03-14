1738ccded80e6c8612b72b298f8daddcb0f38711
Felix <jonfelixrico@gmail.com>
2024-03-15 01:48:54 +0800

    Create new hooks

--- client/src/components/room/hooks/room-socket.hook.ts
+++ client/src/components/room/hooks/room-socket.hook.ts
@@ -3,6 +3,40 @@ import { BroadcastPayload, RoomSocketCode, RoomSocketEvent } from "../../../typi
 import { useCallback, useEffect, useState } from "react"
 import { useLoaderData } from "react-router-dom"
 
+export function useSendMessage () {
+  const socket = useRoomSocket()
+
+  return useCallback((code: RoomSocketCode, payload: unknown) => {
+    socket.emit(RoomSocketEvent.BROADCAST, {
+      code,
+      payload
+    } as BroadcastPayload)
+    console.debug('Sent message with code %s and payload %o', code, payload)
+  }, [socket])
+}
+
+export function useMessageEffect <T>(code: RoomSocketCode, handler: (payload: T) => void, dependencies: unknown[]) {
+  const socket = useRoomSocket()
+
+  useEffect(() => {
+    function internalHandler (sckPayload: BroadcastPayload<T>) {
+      if (sckPayload?.code !== code) {
+        return
+      }
+
+      handler(sckPayload.payload)
+    }
+
+    socket.on(RoomSocketEvent.BROADCAST, internalHandler)
+    console.debug('Created socket.io listener for %s', RoomSocketEvent.BROADCAST)
+
+    return () => {
+      console.debug('Removed socket.io listener for %s', RoomSocketEvent.BROADCAST)
+      socket.off(RoomSocketEvent.BROADCAST, internalHandler)
+    }
+  }, [socket, handler, code, ...dependencies])
+}
+
 export function useRoomSocketManager () {
   const socket = useRoomSocket()
   
