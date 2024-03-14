ffc613a624e5700ac5e52337599a27b8a73fe18b
Felix <jonfelixrico@gmail.com>
2024-03-15 01:57:01 +0800

    Add more payload data

--- client/src/typings/room-socket-code.types.ts
+++ client/src/typings/room-socket-code.types.ts
@@ -1,8 +1,14 @@
+import { Point } from "./geometry.types"
+import { PathData } from "./pad.types"
+
 export enum RoomSocketCode {
   JOINED = 'JOINED',
   USER_DATA = 'USER_DATA',
   CONN_LIST = 'CONN_LIST',
-  CONN_ACTIVITY = 'CONN_ACTIVITY'
+  CONN_ACTIVITY = 'CONN_ACTIVITY',
+
+  PATH_CREATING = 'PATH_CREATING',
+  PATH_CREATED = 'PATH_CREATED'
 }
 
 export enum RoomSocketEvent {
@@ -23,3 +29,16 @@ export interface UserDataPayload {
   uuid: string
   name: string
 }
+
+export interface PathCreatingStartPayload extends PathData {
+  id: string
+  counter: 0
+}
+
+export interface PathCreatingPayload {
+  id: string
+  counter: number
+  point: Point
+}
+
+export type PathCreated = PathData
