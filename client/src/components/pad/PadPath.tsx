04233e444b3ea163521a6573910523445cb31ac3
Felix <jonfelixrico@gmail.com>
2024-03-15 01:40:56 +0800

    Use useMemo

--- client/src/components/pad/PadPath.tsx
+++ client/src/components/pad/PadPath.tsx
@@ -1,19 +1,24 @@
+import { useMemo } from "react"
 import { PathData } from "../../typings/pad.types"
 
 export default function PadPath ({ value }: { value: PathData}) {
   const { points, color, thickness } = value
 
-  const transformed: string[] = []
+  const pathCommands = useMemo(() => {
+    const transformed: string[] = []
 
-  const firstPoint = points[0]
-  transformed.push(`M ${firstPoint.x},${firstPoint.y}`)
+    const firstPoint = points[0]
+    transformed.push(`M ${firstPoint.x},${firstPoint.y}`)
 
-  for (const { x, y } of points) {
-    transformed.push(`L ${x},${y}`)
-  }
+    for (const { x, y } of points) {
+      transformed.push(`L ${x},${y}`)
+    }
+
+    return transformed.join('\n')
+  }, [points])
 
   return <path
-    d={transformed.join('\n')}
+    d={pathCommands}
     stroke={color}
     strokeWidth={thickness}
     fill="none"
