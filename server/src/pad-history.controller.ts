12bac06b52d30ecc3b7a62eec875f4b64e84e1d8
Felix <jonfelixrico@gmail.com>
2024-03-16 01:42:31 +0800

    Implement the functionality

--- server/src/pad-history.controller.ts
+++ server/src/pad-history.controller.ts
@@ -14,10 +14,50 @@ function saveEvent(roomId: string, event: unknown) {
   roomPadEvents[roomId].push(event)
 }
 
+function getEvents(roomId: string, start: number, end: number): unknown[] {
+  const events = roomPadEvents[roomId]
+  if (!events) {
+    return  []
+  }
+
+  return events.slice(start, end)
+}
+
+function getEventCount(roomId: string): number {
+  return roomPadEvents[roomId]?.length ?? 0
+}
+
+type HistoryReq = Partial<{
+  LENGTH: true
+  FETCH: [number, number]
+}>
+
+type HistoryResp = Partial<{
+  LENGTH: number
+  FETCH: unknown[]
+}>
+
 export function padHistoryHandler (socket: Socket, roomId: string) {
   socket.on('PAD', (payload: unknown) => {
     saveEvent(roomId, payload)
   })
 
+  socket.on('PAD_HISTORY', (
+    request: HistoryReq,
+    respond: (response: HistoryResp) => void
+  ) => {
+    if (request.FETCH) {
+      const [start, end] = request.FETCH
+      console.debug('Pad history for room %s was requested: %d to %d', roomId, start, end)
+      return respond({
+        FETCH: getEvents(roomId, start, end)
+      })
+    }
 
+    if (request.LENGTH) {
+      return respond({
+        LENGTH: getEventCount(roomId)
+      })
+    }
+  })
 }
