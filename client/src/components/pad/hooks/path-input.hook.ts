7dc380b6e46969a57da41d93366fa82939e68db3
Felix <jonfelixrico@gmail.com>
2024-03-15 20:03:35 +0800

    Create path input

---
+++ client/src/components/pad/hooks/path-input.hook.ts
@@ -0,0 +1,25 @@
+import { createContext, useContext } from "react";
+import { Point } from "../../../typings/geometry.types";
+
+export interface DrawEvent {
+  point: Point
+  isStart?: boolean
+  isEnd?: boolean
+}
+
+export interface PathInputService {
+  emitDraw (event: DrawEvent): void
+}
+
+const PathInputServiceContext = createContext<PathInputService>({
+  emitDraw: () => {
+    console.warn('emitDraw was called, but context was not provided')
+  }
+})
+
+export const PathInputServiceProvider = PathInputServiceContext.Provider
+
+export function usePathInputService () {
+  const { emitDraw } = useContext(PathInputServiceContext)
+  return emitDraw
+}
