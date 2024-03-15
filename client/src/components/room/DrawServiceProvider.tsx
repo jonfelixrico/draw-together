0c5fbd38460b68492937e5cd868694e2f8e80aa7
Felix <jonfelixrico@gmail.com>
2024-03-15 20:30:43 +0800

    Implement the draw service

--- client/src/components/room/DrawServiceProvider.tsx
+++ client/src/components/room/DrawServiceProvider.tsx
@@ -1,20 +1,56 @@
-import { useCallback, useMemo } from "react";
+import { ReactNode, useCallback, useMemo, useRef } from "react";
 import { useAppDispatch } from "../../store/hooks";
 import { DrawEvent, PathInputService, PathInputServiceProvider } from "../pad/hooks/path-input.hook";
 import { useSendMessage } from "./hooks/room-socket.hook";
+import { PathData } from "../../typings/pad.types";
+import { PadActions } from "../../store/pad.slice";
+import { nanoid } from "nanoid";
 
-export default function DrawServiceProvider () {
+export default function DrawServiceProvider (props: {
+  children: ReactNode
+}) {
   const sendMessage = useSendMessage()
   const dispatch = useAppDispatch()
+  const draftRef = useRef<PathData | null>(null)
 
   const handleDraw = useCallback((event: DrawEvent) => {
     if (event.isStart) {
+      const newDraft: PathData = {
+        points: [event.point],
+        color: 'red',
+        thickness: 5,
+        timestamp: Date.now(),
+        id: nanoid()
+      }
 
-    } else if (event.isEnd) {
-
-    } else {
-
+      dispatch(PadActions.setDraftPath(newDraft))
+      draftRef.current = newDraft
+      return
     }
+
+    const draft = draftRef.current
+    if (!draft) {
+      console.warn('No drafts were found for non-new draw event')
+      return
+    }
+
+    const updated = {
+      ...draft,
+      points: [
+        ...draft.points,
+        event.point
+      ]
+    }
+    
+    if (event.isEnd) {
+      dispatch(PadActions.setPath(updated))
+      dispatch(PadActions.removeDraftPath(draft.id))
+    } else {
+      // reaching this line means that we're processing regular move events
+      dispatch(PadActions.setDraftPath(updated))
+    }
+
+    draftRef.current = updated
   }, [sendMessage, dispatch])
 
   const service: PathInputService = useMemo(() => {
@@ -24,6 +60,6 @@ export default function DrawServiceProvider () {
   }, [handleDraw])
 
   return <PathInputServiceProvider value={service}>
-
+    {props.children}
   </PathInputServiceProvider>
 }
