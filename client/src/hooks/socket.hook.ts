5304b5a6b3e848d4d6413f85e2a9436b5e8d969b
Felix <jonfelixrico@gmail.com>
2024-03-16 01:07:23 +0800

    Impl useSocketOn

---
+++ client/src/hooks/socket.hook.ts
@@ -0,0 +1,25 @@
+import { SocketEventPayload, SocketEventType } from "@/typings/socket.types";
+import { useEffect } from "react";
+import { Socket } from "socket.io-client";
+
+export function useSocketOn<T>(
+  socket: Socket,
+  eventType: SocketEventType,
+  eventCode: string,
+  handler: (payload: T) => void
+) {
+  useEffect(() => {
+    const wrapper = (ePayload?: SocketEventPayload<T>) => {
+      if (ePayload?.code !== eventCode) {
+        return
+      }
+
+      handler(ePayload.payload)
+    }
+
+    socket.on(eventType, wrapper)
+    return () => {
+      socket.off(eventType, wrapper)
+    }
+  }, [socket, eventType, eventCode, handler])
+}
