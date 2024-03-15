aea96eb75f44a67968eb5d7afca5c221ad47a5a0
Felix <jonfelixrico@gmail.com>
2024-03-15 23:01:28 +0800

    Use slider

--- client/src/components/pad/PadPathControl.tsx
+++ client/src/components/pad/PadPathControl.tsx
@@ -3,23 +3,38 @@ import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { PathColor } from '../../typings/pad.types';
-import { PadPathActions, selectColor } from '../../store/pad-path.slice';
+import { PadPathActions, selectColor, selectThickness } from '../../store/pad-path.slice';
+import Slider from '@/re-export/Slider';
 
 export default function PadPathControl () {
   const dispatch = useAppDispatch()
   
   const color = useAppSelector(selectColor)
+  const thickness = useAppSelector(selectThickness)
 
   function setColor (color: PathColor) {
     dispatch(PadPathActions.setColor(color))
   }
 
+  function setThickness (thickness: number) {
+    dispatch(PadPathActions.setThickness(thickness))
+  }
+
   return <Row>
-    <Col>
+    <Col xs="auto">
       <Form.Control
         type='color'
         onChange={event => setColor(event.target.value)}
         value={color}
+      />
+    </Col>
+
+    <Col>
+      <Slider
+        value={thickness}
+        onChangeComplete={value => setThickness(value as number)}
+        min={1}
+        max={20}
       />
     </Col>
   </Row>
