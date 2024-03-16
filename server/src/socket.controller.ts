87b66e160118bfc5b910fb2ea55e4bd52fde837f
Felix <jonfelixrico@gmail.com>
2024-03-16 18:03:57 +0800

    Run the formatter

--- server/src/socket.controller.ts
+++ server/src/socket.controller.ts
@@ -1,7 +1,7 @@
-import { padHistoryHandler } from "./pad-history.controller"
-import { Server, Socket } from "socket.io"
+import { padHistoryHandler } from './pad-history.controller'
+import { Server, Socket } from 'socket.io'
 
-function getQSData ({ request }: Socket) {
+function getQSData({ request }: Socket) {
   /*
    * No need to provide a real base URL since we're only using `url` to extract the query
    * params from the relative path provided by request.url
@@ -11,7 +11,7 @@ function getQSData ({ request }: Socket) {
     // TODO use the `qs` library for this
     roomId: url.searchParams.get('roomId'),
     clientId: url.searchParams.get('clientId') || 'UNKNOWN_ID',
-    name: url.searchParams.get('name') || 'UNKNOWN_NAME'
+    name: url.searchParams.get('name') || 'UNKNOWN_NAME',
   }
 }
 
@@ -26,24 +26,27 @@ type RoomDict = {
   }
 }
 
-export function socketIOHandler (io: Server) {
+export function socketIOHandler(io: Server) {
   const rooms: RoomDict = {}
 
   io.on('connect', (socket: Socket) => {
     const { roomId, clientId, name } = getQSData(socket)
-  
+
     console.debug('Socket %s has connected', clientId)
-  
+
     if (!roomId) {
       socket.emit('ERROR', 'NO_ROOM_ID')
       socket.disconnect()
-      console.warn('Disconnected socket %s, resason: no room ID found in query params', clientId)
+      console.warn(
+        'Disconnected socket %s, resason: no room ID found in query params',
+        clientId
+      )
       return
     }
-  
+
     // TODO add checking for unknown clientId
     // TODO add checking for unknown name
-  
+
     // TODO add checking for nonexistent rooms
 
     socket.join(roomId)
@@ -51,9 +54,9 @@ export function socketIOHandler (io: Server) {
       code: 'CONN_ACTIVITY',
       payload: {
         id: clientId,
-        name,        
-        action: 'join'
-      }
+        name,
+        action: 'join',
+      },
     })
     console.debug('Connected socket %s to room %s', clientId, roomId)
 
@@ -64,7 +67,7 @@ export function socketIOHandler (io: Server) {
     if (!rooms[roomId][clientId]) {
       rooms[roomId][clientId] = {
         id: clientId,
-        name
+        name,
       }
     }
 
@@ -74,33 +77,43 @@ export function socketIOHandler (io: Server) {
         payload: {
           id: clientId,
           name,
-          action: 'leave'
-        }
+          action: 'leave',
+        },
       })
       delete rooms[roomId][clientId]
 
       console.debug('Client %s has left', clientId)
     })
 
+    socket.on(
+      'SERVER',
+      ({ code }: { code: string }, respond: (response: unknown) => void) => {
+        switch (code) {
+          case 'CONN_LIST': {
+            respond(Object.values(rooms[roomId]))
+            console.debug(
+              'Sent %s the list of connected participants',
+              clientId
+            )
+            break
+          }
 
-    socket.on('SERVER', ({ code }: { code: string }, respond: (response: unknown) => void) => {
-      switch (code) {
-        case 'CONN_LIST': {
-          respond(Object.values(rooms[roomId]))
-          console.debug('Sent %s the list of connected participants', clientId)
-          break
-        }
-
-        default: {
-          console.debug('Unknown SERVER_REQ code: %s', code)
-          respond(null)
+          default: {
+            console.debug('Unknown SERVER_REQ code: %s', code)
+            respond(null)
+          }
         }
       }
-    })
-  
+    )
+
     // The purpose of this is to just relay the message to all participants
     socket.on('PAD', (payload: Record<string, unknown>) => {
-      console.debug('Relayed %o to room %s, from client %s', payload, roomId, clientId)
+      console.debug(
+        'Relayed %o to room %s, from client %s',
+        payload,
+        roomId,
+        clientId
+      )
       socket.broadcast.to(roomId).emit('PAD', payload)
     })
 
