b880b7f8c81c2b5636ac500d7d01304f4faaf2fe
Felix <jonfelixrico@gmail.com>
2024-03-17 21:59:40 +0800

    Create new types

--- client/src/typings/server-socket.types.ts
+++ client/src/typings/server-socket.types.ts
@@ -2,3 +2,27 @@ export enum ServerSocketCode {
   CONN_LIST = 'CONN_LIST',
   CONN_ACTIVITY = 'CONN_ACTIVITY',
 }
+
+export const SERVER_SOCKET_EVENT = 'SERVER'
+
+interface ConnectionActivityRespPayload {
+  id: string
+  name: string
+  action: 'leave' | 'join'
+}
+
+interface ConnectionActivityResp {
+  [ServerSocketCode.CONN_ACTIVITY]: ConnectionActivityRespPayload
+}
+
+interface ConnectedUser {
+  id: string
+  name: string
+}
+
+type ConnectedUsersListRespPayload = ConnectedUser[]
+interface ConnectedUsersListResp {
+  [ServerSocketCode.CONN_LIST]: ConnectedUsersListRespPayload
+}
+
+export type ServerResp = ConnectedUsersListResp | ConnectionActivityResp
