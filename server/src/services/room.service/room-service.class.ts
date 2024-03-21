fb8cff501be24fbdf7cf441f64dfe8b50ca1d0f3
Felix <jonfelixrico@gmail.com>
2024-03-22 03:03:12 +0800

    c8: test: test the purging mechanism (#35)
    * Add jest as a dev dependency
    
    * Push results of the jest init
    
    * Init jest via ts-jest
    
    * Kick after 30 mins of inactivity
    
    * Move purging logic to its own method
    
    * Create CONST
    
    * Expose some as public
    
    * Add jest tests
    
    * Add more tests
    
    * Adjust names
    
    * Adjust names
    
    * Make client parallel
    
    * Adjust server routine
    
    * Change test name

--- server/src/services/room.service/room-service.class.ts
+++ server/src/services/room.service/room-service.class.ts
@@ -2,12 +2,21 @@ import { nanoid } from 'nanoid'
 import { CronJob } from 'cron'
 import { Room } from './room.class'
 
+/**
+ * In millis.
+ */
+export const INACTIVITY_THRESHOLD = 1000 * 60 * 30
+
 export class RoomService {
-  /*
+  /**
    * TODO persist room data
    * For simplicity, we're just storing them in-memory for now
+   * 
+   * This is public to be mockable by jest. This should NOT be accessed
+   * outside of tests.
+   * @private
    */
-  private rooms: Record<string, Room> = {}
+  public rooms: Record<string, Room> = {}
   private cronJob: CronJob
 
   constructor() {
@@ -17,19 +26,25 @@ export class RoomService {
      * Its performance will probably be bad if there are hundreds of rooms around.
      * Since the scale of the project is small, we'll choose simplicity over robustness.
      */
-    this.cronJob = new CronJob('* */1 * * * *', () => {
-      const roomIds = Object.keys(this.rooms)
-      const referenceTs = Date.now()
+    this.cronJob = new CronJob('* */1 * * * *', () => this.purgeInactiveRooms())
+  }
 
-      for (const roomId of roomIds) {
-        if (referenceTs - this.rooms[roomId].lastActivityTs < 5000) {
-          return
-        }
+  /**
+   * Exposed as public for jest. Do NOT call in app code.
+   * @private 
+   */
+  purgeInactiveRooms() {
+    const roomIds = Object.keys(this.rooms)
+    const referenceTs = Date.now()
 
-        delete this.rooms[roomId]
-        console.debug('Room %s has been deleted due to inactivity', roomId)
+    for (const roomId of roomIds) {
+      if (referenceTs - this.rooms[roomId].lastActivityTs < INACTIVITY_THRESHOLD) {
+        return
       }
-    })
+
+      delete this.rooms[roomId]
+      console.debug('Room %s has been deleted due to inactivity', roomId)
+    }
   }
 
   createRoom() {
