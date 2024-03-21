d45d2074bd33ff6432731f0b665b43c0268b8e69
Felix <jonfelixrico@gmail.com>
2024-03-22 00:21:58 +0800

    Use encapsulation

--- server/src/services/room.ts
+++ server/src/services/room.ts
@@ -3,19 +3,27 @@ export class Room {
    * TODO add types for the "abstract" event
    * For now, we'll just use unknown since this class doesn't care about what type it is, anyway
    */
-  public history: unknown[] = []
-  public lastActivityTs: number
+  private _history: unknown[] = []
+  private _lastActivityTs: number
 
   constructor(public readonly roomId: string) {
-    this.lastActivityTs = Date.now()
+    this._lastActivityTs = Date.now()
   }
 
   bumpLastActivityTs () {
-    this.lastActivityTs = Date.now()
+    this._lastActivityTs = Date.now()
   }
 
   addToHistory (event: unknown) {
     this.bumpLastActivityTs()
-    this.history.push(event)
+    this._history.push(event)
+  }
+
+  get history () {
+    return this._history
+  }
+
+  get lastActivityTs () {
+    return this._lastActivityTs
   }
 }
