581d80777eff5c56afe1256de13fbb2bef3f2f1b
Felix <jonfelixrico@gmail.com>
2024-03-23 00:15:08 +0800

    Add comment about conscious choice of not hiding the loading spinner

--- client/src/components/home/HomeActionStepHost.tsx
+++ client/src/components/home/HomeActionStepHost.tsx
@@ -2,6 +2,7 @@ import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import { useApiClient } from '@/hooks/api-client.hook'
 import { useNavigate } from 'react-router-dom'
+import { useLoading } from '@/hooks/loading.hook'
 
 interface ResponseBody {
   id: string
@@ -10,12 +11,29 @@ interface ResponseBody {
 export default function HomeActionStepHost() {
   const api = useApiClient()
   const navigate = useNavigate()
+  const { setLoading } = useLoading()
 
   async function createRoomAndNavigate() {
-    const { data } = await api.post<ResponseBody>('room')
-    // TODO handle errors
-    // TODO add spinner
-    navigate(`rooms/${data.id}`)
+    setLoading(true)
+    try {
+      const { data } = await api.post<ResponseBody>('room')
+      
+      navigate(`rooms/${data.id}`)
+      /*
+       * We're not doing setLoading(false) for the success use case because we'll let
+       * the rooms/:id route to clean it up for us.
+       * 
+       * This is also a UX-related measure. If we hide the spinner on the success case and
+       * somehow the spinner in the room page got delayed, the user will be able to perceive
+       * a brief moment of unresponsiveness.
+       * 
+       * Room pag spinner can get delayed if the room page code is still being lazy-loaded.
+       */
+    } catch (e) {
+      // TODO show user-friendly error
+      console.log('Error encoutnered room creation', e)
+      setLoading(false)
+    }
   }
 
   return (
