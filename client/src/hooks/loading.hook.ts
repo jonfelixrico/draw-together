ccd9e8ee46b84c6220e1426a290e1f9cf08e6588
Felix <jonfelixrico@gmail.com>
2024-03-22 23:31:12 +0800

    Create loading hook

---
+++ client/src/hooks/loading.hook.ts
@@ -0,0 +1,15 @@
+import { useAppDispatch } from "@/store/hooks";
+import { UiActions } from "@/store/ui.slice";
+import { useCallback } from "react";
+
+export function useLoading () {
+  const dispatch = useAppDispatch()
+
+  const setLoading = useCallback((isLoading: boolean) => {
+    dispatch(UiActions.setLoading(isLoading))
+  }, [dispatch])
+
+  return {
+    setLoading
+  }
+}
