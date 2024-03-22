4b168e5d603d6e3ebb27d7a059d90d86f3c5f060
Felix <jonfelixrico@gmail.com>
2024-03-22 23:21:02 +0800

    Add loading functionality

--- client/src/providers/LoaderProvider.tsx
+++ client/src/providers/LoaderProvider.tsx
@@ -1,11 +1,24 @@
+import { useAppSelector } from "@/store/hooks";
 import { ReactNode } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import { If, Then } from "react-if";
 
 export default function LoaderProvider ({
   children
 }: {
   children: ReactNode
 }) {
+  const showLoading = useAppSelector(state => state.ui.loading)
+
   return <div className="position-relative">
+    <If condition={showLoading}>
+      <Then>
+        <div className="d-flex flex-column justify-content-center position-absolute h-100 w-100">
+          <Spinner />
+        </div>
+      </Then>
+    </If> 
+
     {children}
   </div>
 }
