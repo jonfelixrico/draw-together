4fc326110220ad56e3d6a37c5239b26151c419b0
Felix <jonfelixrico@gmail.com>
2024-03-24 01:59:29 +0800

    Create slice for socket

---
+++ client/src/store/socket.slice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ConnectedParticipant {
+  id: string
+  isConnected: boolean 
+  name: string
+}
+
+interface SocketSlice {
+  participants: Record<string, ConnectedParticipant>
+}
+
+export const socketSlice = createSlice({
+  name: 'socket',
+  initialState: {
+    participants: {}
+  } as SocketSlice,
+  reducers: {
+    setParticipant: (state, { payload }: PayloadAction<ConnectedParticipant>) => {
+      state.participants[payload.id] = payload
+    },
+
+    setParticipants: (state, { payload }: PayloadAction<ConnectedParticipant[]>) => {
+      for (const participant of payload) {
+        state.participants[participant.id] = participant
+      }
+    }
+  }
+})
+
+export const SocketActions = socketSlice.actions
+export default socketSlice.reducer
