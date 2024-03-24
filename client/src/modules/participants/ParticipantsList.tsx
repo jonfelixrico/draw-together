ace4bf3d0e9da9689567d6646e86057de187bbe2
Felix <jonfelixrico@gmail.com>
2024-03-24 17:02:53 +0800

    Adjust UI of the participant item

--- client/src/modules/participants/ParticipantsList.tsx
+++ client/src/modules/participants/ParticipantsList.tsx
@@ -3,11 +3,28 @@ import { Participant } from './participants.types'
 import { useParticipants } from './participants.hook'
 import { useMemo } from 'react'
 import sortBy from 'lodash/sortBy'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Badge from 'react-bootstrap/Badge'
+import { Else, If, Then } from 'react-if'
 
 function ParticipantItem({ participant }: { participant: Participant }) {
   return <ListGroup.Item>
-    {participant.name}
-    {participant.isConnected}
+    <Row className='justify-content-between align-items-center'>
+      <Col xs="auto">
+        {participant.name}
+      </Col>
+      <Col xs="auto">
+        <If condition={participant.isConnected}>
+          <Then>
+            <Badge bg="success">Online</Badge>
+          </Then>
+          <Else>
+            <Badge bg="secondary">Offline</Badge>
+          </Else>
+        </If>
+      </Col>
+    </Row>
   </ListGroup.Item>
 }
 
