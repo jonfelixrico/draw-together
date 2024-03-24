e3a5d58919b1e0d474c044930c91575fe0cdf15c
Felix <jonfelixrico@gmail.com>
2024-03-24 16:35:41 +0800

    Integrate ParticipantsList to RoomContent

--- client/src/components/room/RoomContent.tsx
+++ client/src/components/room/RoomContent.tsx
@@ -2,24 +2,18 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import {
-  useConnectedUsers,
   useParticipantWatcher,
 } from '@/modules/participants/participants.hook'
-import { useMemo } from 'react'
-import sortBy from 'lodash/sortBy'
 import { useMeasure } from 'react-use'
 import PadContentRenderer from '@/components/pad/PadContentRenderer'
 import { PadInput } from '@/components/pad/PadInput'
 import DrawServiceProvider from './service/DrawServiceProvider'
 import { usePathSocketWatcher } from './service/socket-path-watcher.hook'
 import PadPathControl from '@/components/pad/PadPathControl'
+import ParticipantsList from '@/modules/participants/ParticipantsList'
 
 export default function RoomContent() {
   useParticipantWatcher()
-  const users = useConnectedUsers()
-  const userList = useMemo(() => {
-    return sortBy(Object.values(users), ({ name }) => name)
-  }, [users])
   const [ref, dimensions] = useMeasure<HTMLDivElement>()
   usePathSocketWatcher()
 
@@ -35,15 +29,7 @@ export default function RoomContent() {
       <Row className="h-100">
         <Col xs="2" className="py-2">
           <div>Connected Users</div>
-          <ol data-cy="participants">
-            {userList.map(({ id, name }) => {
-              return (
-                <li data-cy={id} key={id}>
-                  {name}
-                </li>
-              )
-            })}
-          </ol>
+          <ParticipantsList />
         </Col>
         <Col>
           <Row className="flex-column h-100 gy-2">
