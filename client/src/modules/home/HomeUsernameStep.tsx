9908dba87f36387f6b58cdf3e4c67deab49b7b83
Felix <jonfelixrico@gmail.com>
2024-03-25 23:44:29 +0800

    c6: feat: improve UX of the home page steps (#40)

---
+++ client/src/modules/home/HomeUsernameStep.tsx
@@ -0,0 +1,46 @@
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Stack from 'react-bootstrap/Stack'
+import Card from 'react-bootstrap/Card'
+import { useState } from 'react'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+export default function HomeUsernameStep(props: {
+  onInput: (value: string) => void
+}) {
+  const [value, setValue] = useState('')
+
+  return (
+    <Row className="justify-content-center">
+      <Col xs md="10" lg="6">
+        <Card data-cy="username-step">
+          <Card.Body>
+            <div className="text-center mb-3">
+              <Card.Title as="h4">Who are you?</Card.Title>
+              <div className="text-secondary">Please enter a name</div>
+            </div>
+
+            <Stack gap={1}>
+              <Form.Control
+                placeholder="Pick a name"
+                name="name"
+                type="text"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+
+              <Button
+                type="submit"
+                disabled={!value}
+                onClick={() => props.onInput(value)}
+              >
+                Confirm Name
+              </Button>
+            </Stack>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
+}
--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,8 +1,8 @@
 import Container from 'react-bootstrap/Container'
 import { useLocalStorage } from 'react-use'
 import { If, Then, Else } from 'react-if'
-import HomeActionStep from '@/components/home/HomeActionStep'
-import HomeUsernameStep from '@/components/home/HomeUsernameStep'
+import HomeActionStep from '@/modules/home/HomeActionStep'
+import HomeUsernameStep from '@/modules/home/HomeUsernameStep'
 
 export function Component() {
   const [username, setUsername, clearUsername] = useLocalStorage(
