d6350e8117654caa012142b45725c5b3038aa092
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-30 18:41:33 +0800

    Relocate

--- client/src/modules/pad-socket/pad-socket.types.ts
+++ client/src/modules/pad-socket/pad-socket.types.ts
@@ -1,5 +1,5 @@
-import { Point } from '../../typings/geometry.types'
-import { PathData } from '../../typings/pad.types'
+import { Point } from '@/modules/common/geometry.types'
+import { PathData } from '@/modules/pad/pad.types'
 
 export const PAD_SOCKET_EVENT = 'PAD'
 
