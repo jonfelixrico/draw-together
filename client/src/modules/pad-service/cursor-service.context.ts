dbbedee2700bc9535e43b01b8bcb8febafb700b0
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-30 23:46:18 +0800

    Add code for cursor service context

---
+++ client/src/modules/pad-service/cursor-service.context.ts
@@ -0,0 +1,18 @@
+import { Point } from '@/modules/common/geometry.types'
+import { createContext, useContext } from 'react'
+
+export interface CursorService {
+  setUserCursor(point: Point): void
+}
+
+const CursorServiceContext = createContext<CursorService>({
+  setUserCursor: () => {
+    console.warn('Cursor service is not provided, but setUserCursor was called')
+  },
+})
+
+export const CursorServiceProvider = CursorServiceContext.Provider
+
+export function useCursorService() {
+  return useContext(CursorServiceContext)
+}
