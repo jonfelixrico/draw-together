e5785cbd07dd81d0cb8afcf29c9f2b26b6f29dcd
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-30 23:32:08 +0800

    Integrate to pad

--- client/src/modules/pad/Pad.tsx
+++ client/src/modules/pad/Pad.tsx
@@ -1,6 +1,7 @@
 import { Dimensions } from '@/modules/common/geometry.types'
 import PadPathsRenderer from '@/modules/pad/PadPathsRenderer'
 import { PadInput } from '@/modules/pad/PadInput'
+import PadCursorsRenderer from '@/modules/pad/PadCursorsRenderer'
 
 export function Pad({ dimensions }: { dimensions: Dimensions }) {
   return (
@@ -11,6 +12,10 @@ export function Pad({ dimensions }: { dimensions: Dimensions }) {
 
       <div className="position-absolute" style={{ zIndex: 1 }}>
         <PadPathsRenderer dimensions={dimensions} />
+      </div>
+
+      <div className="position-absolute" style={{ zIndex: 2 }}>
+        <PadCursorsRenderer dimensions={dimensions} />
       </div>
     </div>
   )
