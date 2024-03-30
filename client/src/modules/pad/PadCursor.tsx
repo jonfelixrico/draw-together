2da3725b2606b6cdd4eebd560036387196eae8a5
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-30 23:01:35 +0800

    Create cursor component

---
+++ client/src/modules/pad/PadCursor.tsx
@@ -0,0 +1,53 @@
+import { Dimensions, Point } from '@/modules/common/geometry.types'
+
+const CURSOR_STROKE_WIDTH = 2
+function Cursor({ diameter }: { diameter: number }) {
+  const radius = diameter / 2
+  const size = diameter + CURSOR_STROKE_WIDTH * 2
+
+  return (
+    <svg width={size} height={size}>
+      <circle
+        cx={radius + CURSOR_STROKE_WIDTH}
+        cy={radius + CURSOR_STROKE_WIDTH}
+        strokeWidth={CURSOR_STROKE_WIDTH}
+        stroke="black"
+        fill="none"
+        r={radius}
+      />
+    </svg>
+  )
+}
+
+export default function PadCursor({
+  dimensions,
+  point,
+  diameter,
+  label,
+}: {
+  dimensions: Dimensions
+  point: Point
+  diameter: number
+  label: string
+}) {
+  return (
+    <div style={dimensions}>
+      <div
+        style={{
+          transformOrigin: 'center',
+          transform: `translate(${point.x}px, ${point.y}px)`,
+        }}
+      >
+        <Cursor diameter={diameter} />
+      </div>
+      <div
+        style={{
+          transformOrigin: 'left center',
+          transform: `translate(${point.x}px, ${point.y}px)`,
+        }}
+      >
+        {label}
+      </div>
+    </div>
+  )
+}
