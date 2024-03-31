4edbace68a7e118fe4a6dea82d742beb14ab2c1f
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-31 14:55:39 +0800

    Use absolute path

--- client/src/modules/pad/PadInput.tsx
+++ client/src/modules/pad/PadInput.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from 'react'
 import { Dimensions } from '@/modules/common/geometry.types'
 import Draggable, { DraggableEvent } from './Draggable'
-import { usePathInputService } from '../pad-service/path-input-service.context'
+import { usePathInputService } from '@/modules/pad-service/path-input-service.context'
 
 export function PadInput({ dimensions }: { dimensions: Dimensions }) {
   const { emitDraw } = usePathInputService()
