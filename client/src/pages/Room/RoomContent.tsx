f4d5e465c4f16abb3d9e482668dd976d2541e248
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-31 18:57:53 +0800

    Adjust the layout

--- client/src/pages/Room/RoomContent.tsx
+++ client/src/pages/Room/RoomContent.tsx
@@ -4,11 +4,7 @@ import Col from 'react-bootstrap/Col'
 import { useParticipantWatcher } from '@/modules/participants/participants.hook'
 import { useMeasure } from 'react-use'
 import { usePathSocketWatcher } from '@/modules/pad-socket/socket-path-watcher.hook'
-import PadPathControl from '@/modules/pad/PadPathControl'
 import ParticipantsList from '@/modules/participants/ParticipantsList'
-import { useScreen } from '@/modules/common/screen.hook'
-import { If, Then } from 'react-if'
-import ParticipantsModalButton from '@/modules/participants/ParticipantsModalButton'
 import { PathInputServiceProvider } from '@/modules/pad-service/path-input-service.context'
 import { usePathInputServiceImpl } from '@/modules/pad-service/path-input-service-impl.hook'
 import { Pad } from '@/modules/pad/Pad'
@@ -19,8 +15,6 @@ export default function RoomContent() {
   useParticipantWatcher()
   const [ref, dimensions] = useMeasure<HTMLDivElement>()
   usePathSocketWatcher()
-
-  const screen = useScreen()
 
   const pathInputService = usePathInputServiceImpl()
   const cursorService = useCursorServiceImpl()
@@ -33,46 +27,25 @@ export default function RoomContent() {
         // touch-action: none is required to make drawing work for touchscreen devices
         touchAction: 'none',
       }}
+      fluid
     >
       <Row className="h-100">
-        <If condition={screen.gt.md}>
-          <Then>
-            <Col xs="2" className="py-2" data-cy="participants-drawer">
-              <div className="h5">Participants</div>
-              <ParticipantsList />
-            </Col>
-          </Then>
-        </If>
-        <Col>
-          <Row className="flex-column h-100 gy-2">
-            <Col className="pt-2">
-              {/* Intermediate div is present because we can't easily attach ref to Col */}
-              <div className="h-100 w-100 position-relative border" ref={ref}>
-                <div className="position-absolute">
-                  <PathInputServiceProvider value={pathInputService}>
-                    <CursorServiceProvider value={cursorService}>
-                      <Pad dimensions={dimensions} />
-                    </CursorServiceProvider>
-                  </PathInputServiceProvider>
-                </div>
-              </div>
-            </Col>
-            <Col xs="auto">
-              <Row>
-                <If condition={screen.lt.lg}>
-                  <Then>
-                    <Col xs="auto">
-                      <ParticipantsModalButton />
-                    </Col>
-                  </Then>
-                </If>
+        <Col className="p-0">
+          {/* Intermediate div is present because we can't easily attach ref to Col */}
+          <div className="h-100 w-100 position-relative border" ref={ref}>
+            <div className="position-absolute">
+              <PathInputServiceProvider value={pathInputService}>
+                <CursorServiceProvider value={cursorService}>
+                  <Pad dimensions={dimensions} />
+                </CursorServiceProvider>
+              </PathInputServiceProvider>
+            </div>
+          </div>
+        </Col>
 
-                <Col>
-                  <PadPathControl />
-                </Col>
-              </Row>
-            </Col>
-          </Row>
+        <Col xs="2" className="p-2" data-cy="participants-drawer">
+          <div className="h5">Participants</div>
+          <ParticipantsList />
         </Col>
       </Row>
     </Container>
