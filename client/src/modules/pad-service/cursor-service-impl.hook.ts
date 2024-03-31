eba2d73e8ec8e891e6a76c6d3eee967a551c3966
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-31 15:56:06 +0800

    Integrate to the service

--- client/src/modules/pad-service/cursor-service-impl.hook.ts
+++ client/src/modules/pad-service/cursor-service-impl.hook.ts
@@ -1,12 +1,42 @@
+import {
+  CursorUpdate,
+  CursorUpdatePayload,
+  PAD_TRANSIENT_EVENT_TYPE,
+  PadTransientResponse,
+  PadTransientSocketCode,
+} from '@/modules/pad-socket/pad-transient-socket.types'
 import { useClientId } from '@/modules/common/client-id.hook'
 import { PadActions } from '@/modules/pad-common/pad.slice'
 import { CursorService } from '@/modules/pad-service/cursor-service.context'
-import { useAppDispatch } from '@/store/hooks'
+import { useRoomSocket } from '@/modules/socket/room-socket.hook'
+import { useSocketOn } from '@/modules/socket/socket.hook'
+import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { useCallback } from 'react'
 
 export function useCursorServiceImpl(): CursorService {
   const userId = useClientId()
   const dispatch = useAppDispatch()
+  const diameter = useAppSelector((root) => root.pad.options.thickness)
+
+  const cursorFromOtherUsersHandler = useCallback(
+    (payload: CursorUpdatePayload) => {
+      dispatch(
+        PadActions.setCursor({
+          ...payload,
+          timestamp: Date.now(),
+        })
+      )
+    },
+    [dispatch]
+  )
+
+  const socket = useRoomSocket()
+  useSocketOn<PadTransientResponse>(
+    socket,
+    PAD_TRANSIENT_EVENT_TYPE,
+    PadTransientSocketCode.CURSOR_UPDATE,
+    cursorFromOtherUsersHandler
+  )
 
   const setUserCursor: CursorService['setUserCursor'] = useCallback(
     (point) => {
@@ -17,12 +47,21 @@ export function useCursorServiceImpl(): CursorService {
           timestamp: Date.now(),
         })
       )
+
+      socket.emit(PAD_TRANSIENT_EVENT_TYPE, {
+        CURSOR_UPDATE: {
+          diameter,
+          id: userId,
+          point,
+        },
+      } as CursorUpdate)
     },
-    [dispatch, userId]
+    [dispatch, userId, socket, diameter]
   )
 
   const clearUserCursor: CursorService['clearUserCursor'] = useCallback(() => {
     dispatch(PadActions.clearCursor(userId))
+    // No socket operation is involved because we just let inactive cursors of other users expire (timestamp age)
   }, [dispatch, userId])
 
   return {
