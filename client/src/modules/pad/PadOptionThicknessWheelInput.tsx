186ae255205ceacc2fc116408b7e75c185143424
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-31 17:46:17 +0800

    Add logging

--- client/src/modules/pad/PadOptionThicknessWheelInput.tsx
+++ client/src/modules/pad/PadOptionThicknessWheelInput.tsx
@@ -9,16 +9,23 @@ export default function PadOptionThicknessWheelInput({
 }) {
   const dispatch = useAppDispatch()
 
+  function increment(value: number) {
+    console.debug('Thickness incrementd by %d', value)
+    dispatch(PadActions.incrementThickness(value))
+  }
+
   const wheelHandler: React.WheelEventHandler<HTMLDivElement> = ({
     deltaY,
   }) => {
+    // Mouse was scrolled down
     if (deltaY > 0) {
-      dispatch(PadActions.incrementThickness(1))
+      increment(-1)
       return
     }
 
+    // Mouse was scrolled up
     if (deltaY < 0) {
-      dispatch(PadActions.incrementThickness(-1))
+      increment(1)
       return
     }
   }
