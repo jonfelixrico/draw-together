b8eefefb71e071317f3697d40be357d74c5850d0
Jon Felix Rico <jonfelixrico@gmail.com>
2024-03-31 18:50:03 +0800

    Create new controls component

---
+++ client/src/modules/pad/PadOptionsControls.tsx
@@ -0,0 +1,35 @@
+import { PadActions } from '@/modules/pad-common/pad.slice'
+import { useAppDispatch, useAppSelector } from '@/store/hooks'
+import Stack from 'react-bootstrap/Stack'
+import { HexColorPicker } from 'react-colorful'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Slider from '@/lib/Slider'
+
+export default function PadOptionsControls() {
+  const color = useAppSelector((root) => root.pad.options.color)
+  const thickness = useAppSelector((root) => root.pad.options.thickness)
+  const dispatch = useAppDispatch()
+
+  return (
+    <Stack>
+      <Row>
+        <Col xs={2}>Thickness</Col>
+        <Col>
+          <Slider
+            value={thickness}
+            onChange={(newThickness) =>
+              dispatch(PadActions.setThickness(newThickness as number))
+            }
+            min={1}
+            max={100}
+          />
+        </Col>
+      </Row>
+      <HexColorPicker
+        color={color}
+        onChange={(newColor) => dispatch(PadActions.setColor(newColor))}
+      />
+    </Stack>
+  )
+}
