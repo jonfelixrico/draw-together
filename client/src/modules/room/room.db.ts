6f383ecbab854aecf8b8697da793c0c0ba0c988e
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-02 21:34:38 +0800

    Create room db

---
+++ client/src/modules/room/room.db.ts
@@ -0,0 +1,19 @@
+import { Room } from '@/modules/room/room.types'
+import Dexie, { Table } from 'dexie'
+
+export interface RoomDbRecord extends Room {
+  lastOpened: number
+}
+
+class RoomDb extends Dexie {
+  rooms!: Table<RoomDbRecord>
+
+  constructor() {
+    super('room-db')
+    this.version(1).stores({
+      room: 'id',
+    })
+  }
+}
+
+export const roomDb = new RoomDb()
