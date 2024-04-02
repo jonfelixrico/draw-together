2d81777a93398f93934b97b6b8c880efd525cf49
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-02 21:34:43 +0800

    Create history action

---
+++ client/src/modules/home/HomeActionStepHistory.tsx
@@ -0,0 +1,22 @@
+import Card from 'react-bootstrap/Card'
+import { Else, If, Then } from 'react-if'
+import { useLiveQuery } from 'dexie-react-hooks'
+import { roomDb } from '@/modules/room/room.db'
+
+export default function HomeActionStepHistory() {
+  const rooms = useLiveQuery(async () => {
+    return await roomDb.rooms.toArray()
+  })
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Join History</Card.Title>
+        <If condition={!rooms?.length}>
+          <Then>{rooms!.map((room) => JSON.stringify(room))}</Then>
+          <Else>No items</Else>
+        </If>
+      </Card.Body>
+    </Card>
+  )
+}
