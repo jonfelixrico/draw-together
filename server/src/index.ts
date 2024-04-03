04f63dc7347080063bebe0c514df4eca9cf0f533
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-03 21:48:31 +0800

    Fix the route

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,7 +40,7 @@ app.get('/room/:roomId', (req, res) => {
   })
 })
 
-app.get('/room/:roomId/events', (req, res) => {
+app.get('/room/:roomId/events/length', (req, res) => {
   const { roomId } = req.params
 
   const room = roomService.getRoom(roomId)
