f9fdc35f905ae25dd8ef482ce5180d706fc09c5f
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-03 23:52:45 +0800

    Create delete buttons

--- client/src/modules/home/HomeActionStepHistory.tsx
+++ client/src/modules/home/HomeActionStepHistory.tsx
@@ -2,7 +2,6 @@ import Card from 'react-bootstrap/Card'
 import { Else, If, Then } from 'react-if'
 import { useLiveQuery } from 'dexie-react-hooks'
 import { roomDb } from '@/modules/room/room.db'
-import { Link } from 'react-router-dom'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import ListGroup from 'react-bootstrap/ListGroup'
@@ -13,6 +12,13 @@ export default function HomeActionStepHistory() {
     return await roomDb.rooms.toArray()
   })
 
+  async function deleteHistoryEntry(id: string) {
+    const results = await roomDb.rooms.where('id').equals(id).delete()
+    if (results) {
+      console.info('Room %s has been removed from the history', id)
+    }
+  }
+
   return (
     <Card>
       <Card.Body>
@@ -22,8 +28,12 @@ export default function HomeActionStepHistory() {
           </Col>
 
           <Col xs="auto">
-            <Button variant="secondary" onClick={() => roomDb.rooms.clear()}>
-              Clear
+            <Button
+              variant="danger"
+              size="sm"
+              onClick={() => roomDb.rooms.clear()}
+            >
+              Delete All
             </Button>
           </Col>
         </Row>
@@ -32,10 +42,29 @@ export default function HomeActionStepHistory() {
             <ListGroup>
               {rooms?.map((room) => (
                 <ListGroup.Item key={room.id}>
-                  <div>
-                    <Link to={`/rooms/${room.id}`}>{room.name}</Link>
-                  </div>
-                  <div>Last opened {new Date(room.lastOpened).toString()}</div>
+                  <Row>
+                    <Col>
+                      <div className="h5">{room.name}</div>
+                      <div>
+                        Last opened {new Date(room.lastOpened).toString()}
+                      </div>
+                    </Col>
+                    <Col
+                      xs="auto"
+                      className="d-flex flex-row justify-content-center align-items-center gap-2"
+                    >
+                      <Button
+                        variant="danger"
+                        size="sm"
+                        onClick={() => deleteHistoryEntry(room.id)}
+                      >
+                        Delete
+                      </Button>
+                      <Button href={`/rooms/${room.id}`} size="sm">
+                        Join Room
+                      </Button>
+                    </Col>
+                  </Row>
                 </ListGroup.Item>
               ))}
             </ListGroup>
