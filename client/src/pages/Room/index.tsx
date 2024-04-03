68e318ed86f3f8813157302538c8f61d78881eb8
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-03 23:01:37 +0800

    Use state clear

--- client/src/pages/Room/index.tsx
+++ client/src/pages/Room/index.tsx
@@ -23,9 +23,10 @@ import { UiActions } from '@/modules/ui/ui.slice'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { roomDb } from '@/modules/room/room.db'
-import { PadActions } from '@/modules/pad-common/pad.slice'
 import { PadEventsProvider } from '@/modules/pad-socket/pad-events-v2.context'
 import { useEffect } from 'react'
+import { useAppDispatch } from '@/store/hooks'
+import { PadActions } from '@/modules/pad-common/pad.slice'
 
 enum RoomErrorType {
   NO_USERNAME,
@@ -48,9 +49,6 @@ export const loader: LoaderFunction = async ({ params }) => {
   if (!params.roomId) {
     throw new RoomError(RoomErrorType.UNEXPECTED)
   }
-
-  // TODO implement a better way to do this
-  store.dispatch(PadActions.resetState())
 
   store.dispatch(UiActions.setLoading(true))
   try {
@@ -116,14 +114,16 @@ export function Component() {
     socket: Socket
   }
   const { roomId } = useParams()
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     socket.connect()
 
     return () => {
       socket.disconnect()
+      dispatch(PadActions.resetState())
     }
-  }, [socket])
+  }, [socket, dispatch])
 
   return (
     <PadEventsProvider socket={socket} roomId={roomId!}>
