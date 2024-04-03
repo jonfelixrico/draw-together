2f14f57222b9fa2334ef7b20bae61f508db09b44
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-03 23:07:56 +0800

    Add reset slice

--- client/src/modules/socket/socket.slice.ts
+++ client/src/modules/socket/socket.slice.ts
@@ -5,13 +5,16 @@ interface SocketSlice {
   participants: Record<string, Participant>
 }
 
+const INITIAL_STATE: SocketSlice = {
+  participants: {},
+}
+
 // TODO rename to room slice
 export const socketSlice = createSlice({
   name: 'socket',
-  initialState: {
-    participants: {},
-  } as SocketSlice,
+  initialState: INITIAL_STATE,
   reducers: {
+    resetSlice: () => INITIAL_STATE,
     setParticipant: (state, { payload }: PayloadAction<Participant>) => {
       state.participants[payload.id] = payload
     },
