a15305b08c6ec12b5e07d33338ccdf697b6d4c71
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-06 14:54:20 +0800

    Separate the file

---
+++ server/src/server.ts
@@ -0,0 +1,76 @@
+import express from 'express'
+
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import { socketIOHandler } from './socket-controllers/socket.controller'
+import roomService from './services/room.service'
+import nocache from 'nocache'
+import manifest from '@manifest'
+
+export function initServer() {
+  const app = express()
+  const server = createServer(app)
+  const io = new Server(server)
+
+  app.use(nocache())
+
+  app.get('/', (_, res) => {
+    res.json({
+      version: process.env.VERSION_OVERRIDE || manifest.version,
+    })
+  })
+
+  app.post('/room', (_, res) => {
+    const room = roomService.createRoom()
+
+    res.json({
+      id: room.roomId,
+    })
+  })
+
+  app.get('/room/:roomId', (req, res) => {
+    const { roomId } = req.params
+
+    const room = roomService.getRoom(roomId)
+    if (!room) {
+      return res.sendStatus(404)
+    }
+
+    res.json({
+      id: roomId,
+      name: room.name,
+    })
+  })
+
+  app.get('/room/:roomId/event/length', (req, res) => {
+    const { roomId } = req.params
+
+    const room = roomService.getRoom(roomId)
+    if (!room) {
+      return res.sendStatus(404)
+    }
+
+    res.json({
+      length: room.history.length,
+    })
+  })
+
+  app.get('/room/:roomId/event', (req, res) => {
+    const { roomId } = req.params
+
+    const room = roomService.getRoom(roomId)
+    if (!room) {
+      return res.sendStatus(404)
+    }
+
+    const { start, end } = req.query
+
+    res.json(
+      room.history.slice(parseInt(start as string), parseInt(end as string))
+    )
+  })
+
+  socketIOHandler(io)
+
+  return server
+}
