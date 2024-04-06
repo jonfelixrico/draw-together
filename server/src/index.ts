7ebbc4dbcda8cece9c120e687133bb4b3d3e0a22
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-06 14:19:19 +0800

    Adjust paths

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import { Server } from 'socket.io'
 import { socketIOHandler } from './socket-controllers/socket.controller'
 import roomService from './services/room.service'
 import nocache from 'nocache'
-import manifest from '@/package.json'
+import manifest from '@manifest'
 
 const app = express()
 const server = createServer(app)
