31bf5ca1799b737e27ed7007e55fbb141677e374
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-06 16:37:01 +0800

    Integrate, add tests

--- server/jest/__tests__/socket-join.spec.ts
+++ server/jest/__tests__/socket-join.spec.ts
@@ -1,37 +1,73 @@
-import { Server } from 'http'
-import { Socket, io } from 'socket.io-client'
+import { io } from 'socket.io-client'
 import { testAxios } from '@test/lib/axios'
-import { connectWrapper } from '@test/utils/socket.test-utils'
 import { createAppInstance } from '@test/utils/server.test-utils'
+import { connectWrapperManager } from '@test/utils/socket.test-utils'
 
 describe('socket-join', () => {
-  let server: Server
+  let serverCleanup: () => Promise<void>
+
+  const { connectWrapper, disconnectAll } = connectWrapperManager()
 
   beforeEach(async () => {
-    server = await createAppInstance()
+    const { close } = await createAppInstance()
+    serverCleanup = close
   })
 
   it('allows connecting', async () => {
-    let client: Socket
     const { data } = await testAxios.post<{ roomId: string }>('/room')
     const roomId = data.roomId
 
-    try {
-      client = io('http://localhost:3000', {
+    const client = io('http://localhost:3000', {
+      query: {
+        roomId,
+        name: 'User 1',
+        clientId: 'user-1',
+      },
+    })
+
+    await expect(connectWrapper(client)).resolves.toBeTruthy()
+  })
+
+  it('broadcasts joins to other people', async () => {
+    const { data } = await testAxios.post<{ roomId: string }>('/room')
+    const roomId = data.roomId
+
+    const clientA = await connectWrapper(
+      io('http://localhost:3000', {
         query: {
           roomId,
           name: 'User 1',
           clientId: 'user-1',
         },
       })
+    )
 
-      await expect(connectWrapper(client)).resolves.toBeTruthy()
-    } finally {
-      client!.disconnect()
-    }
+    const connectSpy = jest.fn()
+    clientA.once('SERVER', connectSpy)
+
+    // role: joiner
+    await connectWrapper(
+      io('http://localhost:3000', {
+        query: {
+          roomId,
+          name: 'User 2',
+          clientId: 'user-2',
+        },
+      })
+    )
+
+    expect(connectSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        CONN_ACTIVITY: {
+          id: 'user-2',
+          name: 'User 2',
+        },
+      })
+    )
   })
 
-  afterEach(() => {
-    server!.close()
+  afterEach(async () => {
+    disconnectAll()
+    await serverCleanup!()
   })
 })
