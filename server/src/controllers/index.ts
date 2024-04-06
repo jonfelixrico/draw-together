eb1848bc8fc145c26b740ab90ab6b213f76e6efd
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-06 14:58:57 +0800

    Create room controller

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,7 +1,7 @@
 import { Express } from 'express'
 
-import roomService from '@/services/room.service'
 import manifest from '@manifest'
+import { initRoomControllers } from '@/controllers/room.controller'
 
 export function initRestControllers(app: Express) {
   app.get('/', (_, res) => {
@@ -10,53 +10,5 @@ export function initRestControllers(app: Express) {
     })
   })
 
-  app.post('/room', (_, res) => {
-    const room = roomService.createRoom()
-
-    res.json({
-      id: room.roomId,
-    })
-  })
-
-  app.get('/room/:roomId', (req, res) => {
-    const { roomId } = req.params
-
-    const room = roomService.getRoom(roomId)
-    if (!room) {
-      return res.sendStatus(404)
-    }
-
-    res.json({
-      id: roomId,
-      name: room.name,
-    })
-  })
-
-  app.get('/room/:roomId/event/length', (req, res) => {
-    const { roomId } = req.params
-
-    const room = roomService.getRoom(roomId)
-    if (!room) {
-      return res.sendStatus(404)
-    }
-
-    res.json({
-      length: room.history.length,
-    })
-  })
-
-  app.get('/room/:roomId/event', (req, res) => {
-    const { roomId } = req.params
-
-    const room = roomService.getRoom(roomId)
-    if (!room) {
-      return res.sendStatus(404)
-    }
-
-    const { start, end } = req.query
-
-    res.json(
-      room.history.slice(parseInt(start as string), parseInt(end as string))
-    )
-  })
+  initRoomControllers(app)
 }
