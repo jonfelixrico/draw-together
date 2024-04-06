9288ddc1ed44ed0b42093d562aee1bf1e619f3db
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-06 16:01:13 +0800

    Create initial tests

---
+++ server/jest/__tests__/socket-join.spec.ts
@@ -0,0 +1,42 @@
+import { initServer } from '@/server'
+import { Server } from 'http'
+import { Socket, io } from 'socket.io-client'
+import { testAxios } from '@test/lib/axios'
+import { connectWrapper } from '@test/utils/socket.test-utils'
+
+describe('socket-join', () => {
+  let server: Server
+
+  beforeEach(async () => {
+    server = initServer()
+    await new Promise<void>((resolve) => {
+      server.listen(3000, () => {
+        resolve()
+      })
+    })
+  })
+
+  it('allows connecting', async () => {
+    let client: Socket
+    const { data } = await testAxios.post<{ roomId: string }>('/room')
+    const roomId = data.roomId
+
+    try {
+      client = io('http://localhost:3000', {
+        query: {
+          roomId,
+          name: 'User 1',
+          clientId: 'user-1',
+        },
+      })
+
+      await expect(connectWrapper(client)).resolves.toBeTruthy()
+    } finally {
+      client!.disconnect()
+    }
+  })
+
+  afterEach(() => {
+    server!.close()
+  })
+})
