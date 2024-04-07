5aa01b15d9bbf82a5a7edfb3d9fd1a61303fda9d
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-07 22:30:13 +0800

    Add content test

--- client/src/modules/common/BasicModal.spec.cy.tsx
+++ client/src/modules/common/BasicModal.spec.cy.tsx
@@ -4,18 +4,23 @@ describe('BasicModal', () => {
   it('should be dismissable via the close button', () => {
     const hideFn = cy.spy().as('hideFn')
     cy.mount(
-      <BasicModal
-        show={true}
-        title="Test"
-        data-cy="basic-modal"
-        onHide={hideFn}
-      >
+      <BasicModal show={true} title="Test" onHide={hideFn}>
         Test
       </BasicModal>
     )
 
-    cy.getCy('basic-modal').should('exist')
     cy.getCy('header').find('button').click()
     cy.get('@hideFn').should('have.been.called')
+  })
+
+  it('should show basic content', () => {
+    cy.mount(
+      <BasicModal show={true} title="Title">
+        Content
+      </BasicModal>
+    )
+
+    cy.getCy('title').contains('Title')
+    cy.getCy('body').contains('Content')
   })
 })
