99ffd4e7eb51bd22e2655df23613588e258e8431
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-07 22:27:14 +0800

    Create test

--- client/src/modules/common/BasicModal.spec.cy.tsx
+++ client/src/modules/common/BasicModal.spec.cy.tsx
@@ -2,13 +2,20 @@ import BasicModal from '@/modules/common/BasicModal'
 
 describe('BasicModal', () => {
   it('should be dismissable via the close button', () => {
+    const hideFn = cy.spy().as('hideFn')
     cy.mount(
-      <BasicModal show={true} title="Test" data-cy="basic-modal">
+      <BasicModal
+        show={true}
+        title="Test"
+        data-cy="basic-modal"
+        onHide={hideFn}
+      >
         Test
       </BasicModal>
     )
 
-    cy.getCy('close').click()
     cy.getCy('basic-modal').should('exist')
+    cy.getCy('header').find('button').click()
+    cy.get('@hideFn').should('have.been.called')
   })
 })
