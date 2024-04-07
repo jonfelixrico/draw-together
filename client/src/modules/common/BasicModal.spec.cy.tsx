c0a74fe92951a26b16ed7588cf7764d5bb19de36
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-07 22:33:27 +0800

    Test the buttons

--- client/src/modules/common/BasicModal.spec.cy.tsx
+++ client/src/modules/common/BasicModal.spec.cy.tsx
@@ -23,4 +23,42 @@ describe('BasicModal', () => {
     cy.getCy('title').contains('Title')
     cy.getCy('body').contains('Content')
   })
+
+  it('has a working ok button', () => {
+    const handler = cy.spy().as('handler')
+    cy.mount(
+      <BasicModal
+        show={true}
+        title="Test"
+        onOk={handler}
+        ok={{
+          label: 'Test label',
+        }}
+      >
+        Test
+      </BasicModal>
+    )
+
+    cy.getCy('ok').should('exist').click()
+    cy.get('@handler').should('have.been.called')
+  })
+
+  it('has a working cancel button', () => {
+    const handler = cy.spy().as('handler')
+    cy.mount(
+      <BasicModal
+        show={true}
+        title="Test"
+        onCancel={handler}
+        cancel={{
+          label: 'Test label',
+        }}
+      >
+        Test
+      </BasicModal>
+    )
+
+    cy.getCy('cancel').should('exist').click()
+    cy.get('@handler').should('have.been.called')
+  })
 })
