e0a59cc2a540d1cd9fc4c07a575255abe714acd9
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-09 23:34:42 +0800

    Create component for visually resizing the content

---
+++ client/src/modules/common/TransformScale.tsx
@@ -0,0 +1,28 @@
+import { Dimensions } from '@/modules/common/geometry.types'
+import { ReactNode } from 'react'
+
+export default function TransformScale({
+  children,
+  contentScale,
+  containerDimensions,
+}: {
+  children: ReactNode
+  containerDimensions: Dimensions
+  contentScale: number
+}) {
+  return (
+    <div
+      className="position-relative overflow-clip"
+      style={containerDimensions}
+    >
+      <div
+        className="position-absolute"
+        style={{
+          transform: `scale(${contentScale})`,
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
