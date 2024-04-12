8e3b0a3a12bbf3f51c628d2ba79b02d685b22fcf
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-12 23:59:00 +0800

    Add tests

--- client/src/modules/pad/PadCursors.spec.cy.tsx
+++ client/src/modules/pad/PadCursors.spec.cy.tsx
@@ -7,6 +7,7 @@ describe('PadCursors', () => {
         scale={1}
         defaultDiameter={1}
         currentTime={20000}
+        hideCursorThreshold={5000}
         dimensions={{
           width: 500,
           height: 500,
@@ -23,6 +24,7 @@ describe('PadCursors', () => {
               y: 50,
             },
             diameter: 5,
+            // Lapsed hideCursorThreshold, should not show
             timestamp: 10000,
           },
         }}
@@ -37,7 +39,8 @@ describe('PadCursors', () => {
       <PadCursors
         scale={1}
         defaultDiameter={1}
-        currentTime={10000}
+        currentTime={20000}
+        hideCursorThreshold={5000}
         dimensions={{
           width: 500,
           height: 500,
@@ -54,6 +57,17 @@ describe('PadCursors', () => {
               y: 50,
             },
             diameter: 5,
+            timestamp: 20000,
+          },
+
+          // should not be shown since this has lapsed the threshold
+          user2: {
+            id: 'user2',
+            point: {
+              x: 50,
+              y: 50,
+            },
+            diameter: 5,
             timestamp: 10000,
           },
         }}
@@ -61,5 +75,13 @@ describe('PadCursors', () => {
     )
 
     cy.getCy('cursor').should('have.length', 1)
+    const selector = cy.get('[data-cursor-id=user1]')
+
+    selector.should('exist')
+    selector.findCy('label').should('equal', 'User 2')
+    selector
+      .findCy('cursor-svg')
+      .invoke('outerWidth')
+      .should('eq', 5 + 2)
   })
 })
