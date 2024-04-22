8841733af00573b143a4eca238ad0020dcff256a
Felix <jonfelixrico@gmail.com>
2024-04-22 21:14:33 +0800

    Adjust rectangles renderer

--- client/src/modules/pad/PadRectanglesRenderer.tsx
+++ client/src/modules/pad/PadRectanglesRenderer.tsx
@@ -1,36 +1,46 @@
-import { useMemo } from 'react'
 import { useAppSelector } from '@/store/hooks'
-import sortBy from 'lodash/sortBy'
 import { Dimensions } from '@/modules/common/geometry.types'
-import PadPath from './PadPath'
-import TransformScale from '@/modules/common/TransformScale'
 import PadRectangle from '@/modules/pad/PadRectangle'
+import { useMemo } from 'react'
+import { RectangleData } from '@/modules/pad-common/pad.types'
+
+function Rectangles({
+  values,
+  dimensions,
+}: {
+  values: Record<string, RectangleData>
+  dimensions: Dimensions
+}) {
+  const asArray = useMemo(() => Object.values(values), [values])
+  return (
+    <>
+      {asArray.map((data) => (
+        <div
+          className="position-absolute"
+          key={data.id}
+          style={{
+            zIndex: data.timestamp,
+          }}
+        >
+          <PadRectangle dimensions={dimensions} value={data} />
+        </div>
+      ))}
+    </>
+  )
+}
 
 export default function PadPathsRenderer({
   dimensions,
-  scale,
 }: {
   dimensions: Dimensions
-  scale: number
 }) {
   const draft = useAppSelector((state) => state.pad.draftRectangles)
   const stable = useAppSelector((state) => state.pad.rectangles)
 
-  const pathsToRender = useMemo(() => {
-    const arr = [...Object.values(stable), ...Object.values(draft)]
-
-    return sortBy(arr, ({ timestamp }) => timestamp)
-  }, [draft, stable])
-
   return (
-    <TransformScale dimensions={dimensions} scale={scale}>
-      <div style={dimensions} className="position-relative">
-        {pathsToRender.map((data) => (
-          <div className="position-absolute" key={data.id}>
-            <PadRectangle dimensions={dimensions} value={data} />
-          </div>
-        ))}
-      </div>
-    </TransformScale>
+    <>
+      <Rectangles dimensions={dimensions} values={draft} />
+      <Rectangles dimensions={dimensions} values={stable} />
+    </>
   )
 }
