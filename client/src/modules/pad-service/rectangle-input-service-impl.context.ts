389b61979490a293b2e2b211b0c21b3c1143a8e7
Felix <jonfelixrico@gmail.com>
2024-04-24 21:06:18 +0800

    Implement undo

--- client/src/modules/pad-service/rectangle-input-service-impl.context.ts
+++ client/src/modules/pad-service/rectangle-input-service-impl.context.ts
@@ -1,5 +1,6 @@
 import { PadActions } from '@/modules/pad-common/pad.slice'
 import { RectangleData } from '@/modules/pad-common/pad.types'
+import { useUndoStackService } from '@/modules/pad-common/undo-stack.context'
 import { RectangleInputService } from '@/modules/pad-service/rectangle-input-service.context'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { nanoid } from 'nanoid'
@@ -12,6 +13,7 @@ export function useRectangleInputServiceImpl(): RectangleInputService {
   const draft = useRef<RectangleDraft | null>(null)
 
   const { color, thickness } = useAppSelector(({ pad }) => pad.options)
+  const { push } = useUndoStackService()
 
   const handler: RectangleInputService['emitDraw'] = useCallback(
     (event) => {
@@ -31,13 +33,18 @@ export function useRectangleInputServiceImpl(): RectangleInputService {
       }
 
       if (event.isEnd) {
-        dispatch(PadActions.removeDraftRectangle(draft.current!.id))
+        const id = draft.current!.id
+        dispatch(PadActions.removeDraftRectangle(id))
         dispatch(
           PadActions.setRectangle({
             ...draft.current!,
             focus: event.point,
           })
         )
+
+        push(({ store }) => {
+          store.dispatch(PadActions.removeRectangle(id))
+        })
 
         draft.current = null
         return
@@ -52,7 +59,7 @@ export function useRectangleInputServiceImpl(): RectangleInputService {
       dispatch(PadActions.setDraftRectangle(updated))
       draft.current = updated
     },
-    [dispatch, thickness, color]
+    [dispatch, thickness, color, push]
   )
 
   return {
