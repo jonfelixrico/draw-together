2d59ad249e9581105e19bad906d2411d6f4e2cf3
Felix <jonfelixrico@gmail.com>
2024-04-24 23:00:58 +0800

    Send messages

--- client/src/modules/pad-service/rectangle-input-service-impl.context.ts
+++ client/src/modules/pad-service/rectangle-input-service-impl.context.ts
@@ -2,38 +2,46 @@ import { PadActions } from '@/modules/pad-common/pad.slice'
 import { RectangleData } from '@/modules/pad-common/pad.types'
 import { useUndoStackService } from '@/modules/pad-common/undo-stack.context'
 import { RectangleInputService } from '@/modules/pad-service/rectangle-input-service.context'
+import {
+  PAD_SOCKET_EVENT,
+  PadRequest,
+  PadSocketCode,
+} from '@/modules/pad-socket/pad-socket.types'
+import { useSendMessage } from '@/modules/socket/room-socket.hook'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { nanoid } from 'nanoid'
 import { useCallback, useRef } from 'react'
 
-type RectangleDraft = RectangleData & { changeCount: number }
-
 export function useRectangleInputServiceImpl(): RectangleInputService {
   const dispatch = useAppDispatch()
-  const draft = useRef<RectangleDraft | null>(null)
+  const draft = useRef<RectangleData | null>(null)
 
   const { color, thickness } = useAppSelector(({ pad }) => pad.options)
   const { push } = useUndoStackService()
+  const sendMessage = useSendMessage()
 
   const handler: RectangleInputService['emitDraw'] = useCallback(
     (event) => {
       if (event.isStart) {
-        const data: RectangleDraft = {
+        const data: RectangleData = {
           anchor: event.point,
           focus: event.point,
           color,
           thickness,
           timestamp: Date.now(),
-          changeCount: 0,
+          counter: 1,
           id: nanoid(),
         }
 
         dispatch(PadActions.setDraftRectangle(data))
+        sendMessage(PadSocketCode.SHAPE_DRAFT_START, data)
         draft.current = data
       }
 
       if (event.isEnd) {
         const id = draft.current!.id
+        draft.current!.counter++
+
         dispatch(PadActions.removeDraftRectangle(id))
         dispatch(
           PadActions.setRectangle({
@@ -42,24 +50,36 @@ export function useRectangleInputServiceImpl(): RectangleInputService {
           })
         )
 
-        push(({ store }) => {
+        push(({ store, socket }) => {
           store.dispatch(PadActions.removeRectangle(id))
+          socket.emit(PAD_SOCKET_EVENT, {
+            SHAPE_DELETE: {
+              id,
+            },
+          } as PadRequest)
         })
+
+        sendMessage(PadSocketCode.SHAPE_CREATE, draft.current!)
 
         draft.current = null
         return
       }
 
-      const updated: RectangleDraft = {
+      const updated: RectangleData = {
         ...draft.current!,
         focus: event.point,
-        changeCount: draft.current!.changeCount + 1,
+        counter: draft.current!.counter + 1,
       }
 
       dispatch(PadActions.setDraftRectangle(updated))
+      sendMessage(PadSocketCode.SHAPE_DRAFT_MOVE, {
+        counter: updated.counter,
+        focus: event.point,
+        id: draft.current!.id,
+      })
       draft.current = updated
     },
-    [dispatch, thickness, color, push]
+    [dispatch, thickness, color, push, sendMessage]
   )
 
   return {
