6561754a1c4e910a48fe7cadfa41549e60bd2e0e
Felix <jonfelixrico@gmail.com>
2024-04-24 19:48:27 +0800

    Create PadShapeInput

---
+++ client/src/modules/pad/PadShapeInput.tsx
@@ -0,0 +1,37 @@
+import { useCallback } from 'react'
+import { Dimensions } from '@/modules/common/geometry.types'
+import { DraggableEvent } from './Draggable'
+import { PadBaseInput } from '@/modules/pad/PadBaseInput'
+import { useRectangleInputServiceImpl } from '@/modules/pad-service/rectangle-input-service-impl.context'
+
+export function PadPathInput({
+  dimensions,
+  counterScale,
+}: {
+  dimensions: Dimensions
+  counterScale: number
+}) {
+  const { emitDraw } = useRectangleInputServiceImpl()
+
+  const handleDrag = useCallback(
+    (event: DraggableEvent) => {
+      const { x, y, ...others } = event
+      emitDraw({
+        point: {
+          x,
+          y,
+        },
+        ...others,
+      })
+    },
+    [emitDraw]
+  )
+
+  return (
+    <PadBaseInput
+      dimensions={dimensions}
+      onDrag={handleDrag}
+      counterScale={counterScale}
+    />
+  )
+}
