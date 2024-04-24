daa8f2d8914469fcc7d3fed5a3581bfd4d0bdc58
Felix <jonfelixrico@gmail.com>
2024-04-24 19:41:17 +0800

    Integrate PadBaseInput

--- client/src/modules/pad/PadPathInput.tsx
+++ client/src/modules/pad/PadPathInput.tsx
@@ -1,63 +1,25 @@
 import { useCallback } from 'react'
-import { Dimensions, Point } from '@/modules/common/geometry.types'
-import Draggable, { DraggableEvent } from './Draggable'
+import { Dimensions } from '@/modules/common/geometry.types'
+import { DraggableEvent } from './Draggable'
 import { usePathInputService } from '@/modules/pad-service/path-input-service.context'
+import { PadBaseInput } from '@/modules/pad/PadBaseInput'
 
-export function PadPathInput({
-  dimensions,
-  counterScale,
-}: {
-  dimensions: Dimensions
-  /**
-   * This is pretty much a normal scale value.
-   * Under the hood, this scale value is used to normalize the pad input points emitted.
-   */
-  counterScale: number
-}) {
+export function PadPathInput({ dimensions }: { dimensions: Dimensions }) {
   const { emitDraw } = usePathInputService()
-
-  const normalize = useCallback(
-    (point: Point): Point => {
-      return {
-        x: point.x / counterScale,
-        y: point.y / counterScale,
-      }
-    },
-    [counterScale]
-  )
 
   const handleDrag = useCallback(
     (event: DraggableEvent) => {
-      const { x, y, isStart, isEnd } = event
-
-      if (isStart) {
-        emitDraw({
-          point: normalize({
-            x,
-            y,
-          }),
-          isStart: true,
-        })
-      } else if (isEnd) {
-        emitDraw({
-          point: normalize({
-            x,
-            y,
-          }),
-          isEnd: true,
-        })
-      } else {
-        emitDraw({
-          point: normalize({
-            x,
-            y,
-          }),
-        })
-      }
+      const { x, y, ...others } = event
+      emitDraw({
+        point: {
+          x,
+          y,
+        },
+        ...others,
+      })
     },
-    [emitDraw, normalize]
+    [emitDraw]
   )
 
-  // Removing the cursor since we have implemented our own
-  return <Draggable dimensions={dimensions} onDrag={handleDrag} cursor="none" />
+  return <PadBaseInput dimensions={dimensions} onDrag={handleDrag} />
 }
